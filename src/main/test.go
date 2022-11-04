b7155dd5cd4e672da7f7587ad9a3b91a1e38160a
liuzongdian <liuzongdian.1@bytedance.com>
2022-11-04 20:25:26 +0800

    leader election

--- src/main/test.go
+++ src/main/test.go
@@ -2,37 +2,24 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
-	"sort"
-	"strconv"
+	"sync"
+	"time"
 )
 
-type A struct {
-	Data []*B
-}
-
-type B struct {
-	Val  int
-	Name string
-}
-
 func main() {
-	tmp := []*B{}
-	for i := 0; i < 100; i++ {
-		tmp = append(tmp, &B{
-			Val:  rand.Intn(100000),
-			Name: strconv.FormatInt(rand.Int63n(100000), 10),
-		})
-	}
-	//a := A{
-	//	Data: tmp,
-	//}
-	//t := a.Data
-	sort.Slice(tmp, func(i, j int) bool {
-		return tmp[i].Val > tmp[i].Val
-	})
-
-	for i := 0; i < len(tmp); i++ {
-		fmt.Println(tmp[i])
-	}
+	timer := time.NewTimer(3 * time.Second)
+	start := time.Now()
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		select {
+		case <-timer.C:
+			fmt.Println("done", time.Since(start).Seconds())
+			wg.Done()
+		}
+	}()
+	time.Sleep(2 * time.Second)
+	timer.Stop()
+	timer.Reset(3 * time.Second)
+	wg.Wait()
 }
